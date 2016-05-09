abec22048f4ffb3646b80cf1ea295265631e84bb
navneeth <abc@mail.com>
2016-05-09 11:34:48 -0400

    added igonecase method inthe game class

--- Game.java
+++ Game.java
@@ -6,7 +6,7 @@ public class Game {
     Console console=System.console();
     String answer = console.readLine("what is the capital of Australia ?");
     String capital="Canberra";
-    if(capital.equals(answer)){
+    if(capital.equalsIgnoreCase(answer)){
       System.out.println("Correct answer");
    }else{
     System.out.println("No the answer is "+capital);
