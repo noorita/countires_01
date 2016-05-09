f2e82c5be9c67f75545bbf4e6a027457ca03ebc3
navneeth <abc@mail.com>
2016-05-09 11:48:40 -0400

    reading the capitals.csv file

--- Game.java
+++ Game.java
@@ -1,16 +1,32 @@
 import java.io.Console;
-public class Game {
-	
-   public static void main(String[] args){
-     System.out.println("Welcome to capital game");
-    Console console=System.console();
-    String answer = console.readLine("what is the capital of Australia ?");
-    String capital="Canberra";
-    if(capital.equalsIgnoreCase(answer)){
-      System.out.println("Correct answer");
-   }else{
-    System.out.println("No the answer is "+capital);
-}
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
-}
+public class Game {
+	public static void main(String[] args) {
+		System.out.println("Welcome to capital game");
+		Console console = System.console();
+		try(BufferedReader br = new BufferedReader(new FileReader("capitals.csv"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] cols = line.split(",");
+				String country = cols[0];
+				String capital = cols[1];
+				String answer = console.readLine("What is the capital of " + country + "? ");
+				if (answer.equalsIgnoreCase(capital)) {
+					System.out.println("Correct!");
+				}
+				else {
+					System.out.println("No, the answer is " + capital);	
+				}
+			}
+		}
+	catch (FileNotFoundException e) {
+		e.printStackTrace();
+		} catch (IOException e) {
+		e.printStackTrace();
+		}
+	}
 }
