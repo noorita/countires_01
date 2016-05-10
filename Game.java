08bd82c41210911455e06e489e3e2be0fa92c972
navneeth <abc@mail.com>
2016-05-10 10:15:16 -0400

    added scoring feature,corrects and incorrects

--- Game.java
+++ Game.java
@@ -32,6 +32,6 @@ public class Game {
 		} catch (IOException e) {
 		e.printStackTrace();
 		}
-         System.out.println("you have answer " + "  " + correctCount +"  out of "+ total);
+         System.out.println("you have answer " + "  " + (total-correctCount) + " incorect ");
 	}
 }
